67ef59361567cb83202ac64fea1540696ff8fa6e
unknown <King Lee@KingLee-PC.(none)>
2014-05-09 15:30:27 -0700

    change debug http server code

--- cli/Classes/AppDelegate.cpp
+++ cli/Classes/AppDelegate.cpp
@@ -135,8 +135,10 @@ bool AppDelegate::applicationDidFinishLaunching() {
 			int __val = 9999;
 			this->set_user_data(&__val);
 			json_t* __msg = json_object();
-			json_t* __msg_id = json_integer(MSG_ID::MSG_MAX);
+			json_t* __msg_id = json_integer(MSG_ID::MSG_BEGIN);
+			json_t* __msg_context = json_string("context");
 			json_object_set(__msg, "msg_id", __msg_id);
+			json_object_set(__msg, "context", __msg_context);
 			this->do_post(/*"hi, I will post some data, are you ready!"*/json_dumps(__msg,0));
 			// decref for json object
 			json_decref(__msg_id);
